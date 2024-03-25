67c54d080fe29c4878a81cb8d4520d01322c4624
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-03-25 16:31:49 -0400

    commit 3
    added the variables inside main.js.
    used different names then the ones provided

--- Assignment/Assignment-4/Assignment4-P1/main.js
+++ Assignment/Assignment-4/Assignment4-P1/main.js
@@ -1,1 +1,14 @@
-console.log("code running");
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = ["YapYap THE DESTOYER", "Spartian 117", "Batman" ];
+const insertY = ["The Halo Ring", "Super Earth", "The Void"];
+const insertZ = ["spontaneously combusted", "got pulled into a black hole ", "got taken by Batman"];
+
