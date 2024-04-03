c71a12468ad9ce7e4cbcd79f63726531675b8dd0
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-03 16:06:39 -0400

    Commit 1
    implemented index, style and js from MDN

---
+++ Assignment/Assignment-4/Assignment4-P3/main.js
@@ -0,0 +1,19 @@
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
