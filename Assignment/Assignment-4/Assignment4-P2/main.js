1fe76fa188c1ebe2f8d28bddbe675628d47b4e87
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-03 13:34:51 -0400

    Commit 2
    Declared the array of image flilenames

--- Assignment/Assignment-4/Assignment4-P2/main.js
+++ Assignment/Assignment-4/Assignment4-P2/main.js
@@ -6,6 +6,8 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = [`pic1.jpg`, `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
