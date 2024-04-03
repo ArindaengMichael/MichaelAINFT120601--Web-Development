543a2cd70a7a779d1e75bfbd45946ee7aeaba8c3
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-02 20:57:43 -0400

    Commit 7
    Added the proper formulas for the UK

--- Assignment/Assignment-4/Assignment4-P1/main.js
+++ Assignment/Assignment-4/Assignment4-P1/main.js
@@ -28,8 +28,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 / 14);
+    const temperature =  Math.round((94 - 32) * (5/9));
 
   }
 
