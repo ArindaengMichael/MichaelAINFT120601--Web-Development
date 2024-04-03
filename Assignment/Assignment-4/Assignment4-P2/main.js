89e02cba05bd27baa5c5287b72f2eac3553af117
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-03 14:05:21 -0400

    Commit 3
    Declared the alternate text for each image file

--- Assignment/Assignment-4/Assignment4-P2/main.js
+++ Assignment/Assignment-4/Assignment4-P2/main.js
@@ -10,6 +10,17 @@ const images = [`pic1.jpg`, `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
 
+const alts = {
+    'pic1.jpg' : `Closeup of a persons eye`,
+    'pic2.jpg' : `Rocks with wave lines`,
+    'pic3.jpg' : `White and Purple flowers `,
+    'pic4.jpg' : `Egyption art`,
+    'pic5.jpg' : `large Moth on a large green leaf`,
+
+}
+
+
+
 /* Looping through images */
 
 const newImage = document.createElement('img');
