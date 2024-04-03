e37234a27313c0f421fcdf9d67a31a9066ace5f9
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-03 15:43:27 -0400

    Commit 4
    added the light and darken function to the pictures using the btn and if and else

--- Assignment/Assignment-4/Assignment4-P2/main.js
+++ Assignment/Assignment-4/Assignment4-P2/main.js
@@ -33,3 +33,20 @@ for (const image of images) {
 }
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+
+    if (btnClass === 'dark'){
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 /50%)';
+
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)'
+    }
+})
+
+
