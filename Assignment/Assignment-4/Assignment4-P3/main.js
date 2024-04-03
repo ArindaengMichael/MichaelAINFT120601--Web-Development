e6d91e087ee47719ba21edfb9dd420e5f190393b
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-03 16:14:05 -0400

    Commit 3
    added ball as a class as it is used to represent all the bouncing balls

--- Assignment/Assignment-4/Assignment4-P3/main.js
+++ Assignment/Assignment-4/Assignment4-P3/main.js
@@ -17,3 +17,15 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+class Ball {
+    constructor(x, y, velX, velY, color, size) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
+    }
+  }
+  
