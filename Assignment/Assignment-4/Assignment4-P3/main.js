e386500f3668c74274c2c9b5694ea1d49e22400b
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-03 17:13:05 -0400

    Commit 5
    Hard reset due to issues with provided MDN code

--- Assignment/Assignment-4/Assignment4-P3/main.js
+++ Assignment/Assignment-4/Assignment4-P3/main.js
@@ -1,38 +1,18 @@
-// setup canvas
 
+// Setup canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
+// Function to generate random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
-
+// Function to generate random color
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-    constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
-    }
-  }
-  
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-  
