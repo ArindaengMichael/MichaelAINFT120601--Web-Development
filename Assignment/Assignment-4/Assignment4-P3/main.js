355ff12a9282c04426ee8a43dfdc289fd73a7b70
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-04 00:19:28 -0400

    Commit 8
    because of order of operations added the collisionDetect code from MDN first as well as made an array for the balls

--- Assignment/Assignment-4/Assignment4-P3/main.js
+++ Assignment/Assignment-4/Assignment4-P3/main.js
@@ -32,29 +32,47 @@ class Ball {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
-  
   update() {
     if ((this.x + this.size) >= width) {
       this.velX = -(this.velX);
     }
-  
+
     if ((this.x - this.size) <= 0) {
       this.velX = -(this.velX);
     }
-  
+
     if ((this.y + this.size) >= height) {
       this.velY = -(this.velY);
     }
-  
+
     if ((this.y - this.size) <= 0) {
       this.velY = -(this.velY);
     }
-  
+
     this.x += this.velX;
     this.y += this.velY;
   }
-  
+
+   collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
+// Array for the balls
+const balls = [];
+
+
+
+
 
 
 
