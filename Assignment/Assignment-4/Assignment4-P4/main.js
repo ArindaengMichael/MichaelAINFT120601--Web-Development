4605b7de2260fe36eee9fb89edd3f0cd6b28dd09
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-04 22:32:14 -0400

    Commit 4
    re- added update code and collision detect code with up date to the if statement

--- Assignment/Assignment-4/Assignment4-P4/main.js
+++ Assignment/Assignment-4/Assignment4-P4/main.js
@@ -42,4 +42,40 @@ class Ball extends Shape {
     ctx.fill();
   }
 
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -Math.abs(this.velX);
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
+
