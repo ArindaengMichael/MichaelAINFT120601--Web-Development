bbd01be163088b00a9b7335440f7ce8f35c1a5f9
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-04 23:15:53 -0400

    Commit 7
    added the checkbounds and collisionDetect code for the evil circle so it currently acts like the others

--- Assignment/Assignment-4/Assignment4-P4/main.js
+++ Assignment/Assignment-4/Assignment4-P4/main.js
@@ -105,6 +105,31 @@ class EvilCircle extends Shape {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.stroke();
   }
+  checkBounds() {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+      this.x = width - this.size;
+    }
+
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+      this.y = height - this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          para.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
 
 }
 
