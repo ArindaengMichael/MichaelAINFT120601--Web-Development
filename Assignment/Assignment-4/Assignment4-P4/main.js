0a06af73135adc314cc85234544ccbff53fede81
ArindaengMichael <156015650+ArindaengMichael@users.noreply.github.com>
2024-04-04 23:09:29 -0400

    Commit 4
    Trouble shooting fixed code not running
    added class EivleCircle

--- Assignment/Assignment-4/Assignment4-P4/main.js
+++ Assignment/Assignment-4/Assignment4-P4/main.js
@@ -1,19 +1,15 @@
-// set up canvas
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
+// Function to generate random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
-
+// Function to generate random RGB color value
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
@@ -28,11 +24,10 @@ class Shape {
 }
 
 class Ball extends Shape {
-  constructor(x, y, velX, velY, size, color) {
+  constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
-    this.size = size;
     this.color = color;
-    this.exists = true; 
+    this.size = size;
   }
 
   draw() {
@@ -63,10 +58,9 @@ class Ball extends Shape {
     this.y += this.velY;
   }
 
-  
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball) && ball.exists) {
+      if (!(this === ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -79,3 +73,61 @@ class Ball extends Shape {
   }
 }
 
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener('keydown', (e) => {
+      switch(e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+}
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
